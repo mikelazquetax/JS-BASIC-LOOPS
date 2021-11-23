68deb8af545dac52a4885097650d8001507989f5
Mikel Azqueta <65721391+mikelazquetax@users.noreply.github.com>
2021-11-23 16:33:21 +0100

    "Iteration#3"

---
+++ upgrade-loops/iteracion_3.js
@@ -0,0 +1,12 @@
+const placesToTravel = ['Japon', 'Venecia', 'Murcia', 'Santander', 'Filipinas', 'Madagascar']
+
+
+
+
+function places(){
+
+    for(var value of placesToTravel){
+        console.log(value)
+    }
+}
+places(placesToTravel)
