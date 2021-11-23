c9ad04f1e58f90f7081da8b17b2777dfb19aad60
Mikel Azqueta <65721391+mikelazquetax@users.noreply.github.com>
2021-11-23 16:33:43 +0100

    "Iteration#4"

---
+++ upgrade-loops/iteracion_4.js
@@ -0,0 +1,14 @@
+const alien = {
+    name: 'Wormuck',
+    race: 'Cucusumusu',
+    planet: 'Eden',
+    weight: '259kg'
+};
+
+
+
+for ( var propiedad in alien) {
+
+ console.log(alien[propiedad])
+   
+}
